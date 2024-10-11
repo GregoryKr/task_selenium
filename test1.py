ef9e2419bba4816e2cf25cef3555577e4f9e4a6b
Григорий Краснов <grkrasnov@gmail.com>
2024-10-11 06:15:16 +0300

    initial commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+from tenzorpage import SearchHelper
+import time
+
+def test_tenzor_page(browser):
+    tenzor_page = SearchHelper(browser)
+    tenzor_page.go_to_site()
+    time.sleep(3)
+    tenzor_page.open_tenzor_main_page()
+    time.sleep(5)
+    power_in_people = tenzor_page.check_working_block()
+    assert power_in_people == 'Сила в людях'
+    tenzor_page.move_to_more_details_link()
+    time.sleep(5)
+    photos_size = tenzor_page.check_photos_size()
+    assert photos_size == "Фото одного размера"
